3abd5144f667e4104d18a048da670903dc81944f
manoprasanth-m <65891111+manoprasanth-m@users.noreply.github.com>
2023-03-12 16:50:40 +0530

    Update AddOption.js

--- client/src/components/AddOption.js
+++ client/src/components/AddOption.js
@@ -32,7 +32,7 @@ export default function AddOption (props) {
         // const inp = e.target.elements.option.value.trim()
         // setText({text: inp}) 
         
-        Axios.post(`https://nlsurender.onrender.com/confess`, {confess: text, group: props.head })
+        Axios.post(`https://nlsu.cyclic.app/confess`, {confess: text, group: props.head })
         .then(res => {if(res.status === 200) 
             notifyMessage()
             e.target.elements.option.value = ''
